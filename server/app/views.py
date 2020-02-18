56bc34992968d9736b79fefadb8d834483856c81
tree <2332532718@qq.com>
2020-02-18 17:58:45 +0800

    <feature>: <导航视图>
    <body>
    
    <footer>

--- server/app/views.py
+++ server/app/views.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from typing import Optional, Any, Dict, List, Tuple
-from flask import request, Flask, Blueprint, render_template
+from flask import request, Flask, g, render_template
 from flask.views import MethodView
+
+def load_site_config():
+    if "site" not in g:
+        g.site = {
+            "title": "博客",
+            "name": "treee_",
+        }
+
+def not_found(error: Exception) -> Tuple[str, int]:
+    return render_template("404.html"), 404
 
 class Dev(MethodView):
     def get(self, file: str):
@@ -13,5 +23,8 @@ class IndexView(MethodView):
         return render_template('index.html', name='sadfas')
 
 def init_app(app: Flask) -> None:
-    app.add_url_rule('/index', view_func=IndexView.as_view('index'))
+    app.add_url_rule('/index', view_func=IndexView.as_view('home'))
     app.add_url_rule('/<string:file>', view_func=Dev.as_view('dev'))
+
+    app.register_error_handler(404, not_found)
+    app.before_request(load_site_config)
