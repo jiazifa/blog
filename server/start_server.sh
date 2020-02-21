0d9642b907f7d239e11ee61c4857dd70ba148aa6
tree <2332532718@qq.com>
2020-02-21 22:33:17 +0800

    <chore>: <启动脚本>
    <body>
    
    <footer>

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 FLASK_APP=app.app flask
-PORT=${PORT:-5000}
-exec gunicorn -b :${PORT} --access-logfile - --error-logfile - -k gevent -w 4 "app.app:create_app()"
+PORT=${PORT:-9000}
+exec gunicorn -b :${PORT} -w 4 "runner:application"
